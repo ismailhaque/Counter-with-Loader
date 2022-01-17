606754125e35edf9870c2e84dc7200d1770a0c30
ismailhaque <ismailhaque2956@gmail.com>
2022-01-18 01:59:38 +0600

    project complate

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-const progress_bar = document.querySelector('.progress-bar');
+const progress_bar = document.querySelector('.load');
 const input_number = document.querySelector('#input_number');
 const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const reset = document.querySelector('#reset');
 const counter_result = document.querySelector('#counter_result');
+
+
 
 
 let counter;
@@ -19,9 +21,8 @@ start.addEventListener('click', () => {
 
         counter_result.innerHTML = `<h1> ${counter}</h1>`;
 
-        progress_bar.style.width = `${coun_load(input_number.value, counter)}%`;
-
-        progress_bar.innerText = `${Math.floor(width)}%`;
+        progress_bar.innerHTML = `<div class="progress-bar progress-bar-striped bg-primary" role="progressbar" style="width: ${coun_load(input_number.value, counter)}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+        </div>`;
 
         if (counter < 10) {
             counter_result.innerHTML = `<h1> 0${counter}</h1>`;
@@ -29,20 +30,24 @@ start.addEventListener('click', () => {
             counter_result.innerHTML = `<h1> ${counter}</h1>`;
         }
 
-        // if (counter == 0) {
-        //     clearInterval(clear_count)
-        // }
+        if ( counter == 0 ) {
+            clearTimeout(clear_count)
+        }
 
         counter--;
-        input_number.value = '';
+    
     }, 1000);
 
-    
-    stop.addEventListener('click', () => {
-        clearInterval();
+})
+
+stop.addEventListener('click', () => {
+    clearTimeout(clear_count);
     })
 
-    reset.addEventListener('click', () => {
-        counter_result.innerHTML=`<h1>00</h1>`;
+
+reset.addEventListener('click', () => {
+    counter = "00";
+    counter_result.innerHTML=`<h1>${counter}</h1>`;
+    clearTimeout(clear_count);
+    progress_bar.innerHTML = ``;
     })
-})
